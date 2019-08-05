2d51940e315eafc9473b49f6b98d90eaed904e3a
woxbwo <woxbwo@163.com>
2019-08-05 16:57:24 +0800

    添加Springboot2.x整合apache-active5.x

---
+++ is-springboot-task/src/main/java/com/xb/service/springboot/task/TaskApplication.java
@@ -0,0 +1,10 @@
+package com.xb.service.springboot.task;
+
+/**
+ * @ClassName: TaskApplication
+ * @Description:
+ * @Author: Coding_wxb
+ * @Date 2019.08.02 14:58
+ */
+public class TaskApplication {
+}
